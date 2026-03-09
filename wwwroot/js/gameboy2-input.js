43f7f700561430383c220fd4f1e469056682845f
sussybocca <babyyodacutefry@gmail.com>
2026-03-09 01:27:20 +0000

    ad d files

---
+++ wwwroot/js/gameboy2-input.js
@@ -0,0 +1,37 @@
+window.gameboy2Emulator = {
+    sendKey: (iframe, key, action) => {
+        const iframeWindow = iframe.contentWindow;
+        if (!iframeWindow) return;
+        iframe.focus();
+
+        const keyMap = {
+            'ArrowUp': { key: 'ArrowUp', code: 'ArrowUp', keyCode: 38 },
+            'ArrowDown': { key: 'ArrowDown', code: 'ArrowDown', keyCode: 40 },
+            'ArrowLeft': { key: 'ArrowLeft', code: 'ArrowLeft', keyCode: 37 },
+            'ArrowRight': { key: 'ArrowRight', code: 'ArrowRight', keyCode: 39 },
+            'a': { key: 'a', code: 'KeyA', keyCode: 65 },
+            'b': { key: 'b', code: 'KeyB', keyCode: 66 },
+            'Start': { key: 'Enter', code: 'Enter', keyCode: 13 },
+            'Select': { key: ' ', code: 'Space', keyCode: 32 }
+        };
+
+        const props = keyMap[key] || { key: key, code: key, keyCode: key.charCodeAt(0) };
+        const eventType = action === 'down' ? 'keydown' : 'keyup';
+
+        const event = new KeyboardEvent(eventType, {
+            key: props.key,
+            code: props.code,
+            keyCode: props.keyCode,
+            which: props.keyCode,
+            bubbles: true,
+            cancelable: true,
+            composed: true
+        });
+
+        iframeWindow.dispatchEvent(event);
+        iframeWindow.document.activeElement?.dispatchEvent(event);
+    },
+    focus: (iframe) => {
+        iframe.focus();
+    }
+};
